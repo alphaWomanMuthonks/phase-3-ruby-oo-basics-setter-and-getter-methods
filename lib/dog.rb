174af23251f8925125cd086f0b98d66a12c99a4a
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-10-20 16:24:24 +0300

    learning setter and getter

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,10 @@
 class Dog
+    attr_accessor :name
+    attr_accessor :breed
 
+    def names=(name ,breed)
+        @name = name
+        @breed = breed
+    
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
+    
+    attr_accessor :name
+    attr_accessor :job
+
+    def names=(name,job)
+        @name= name
+        @job= job
+    end
 
 end
